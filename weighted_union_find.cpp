b3e4172cada7f1727df3593afe93d12c729e1e7a
_ <>
2021-12-25 06:00:09 +0900

    fix weighted union find for non-abelian groups

--- weighted_union_find.cpp
+++ weighted_union_find.cpp
@@ -39,21 +39,22 @@ struct weighted_union_find {
 
   int leader(int a) {
     assert(0 <= a && a < _n);
-    int x = a;
-    S w = e();
-    while (parent_or_size[x] >= 0) {
-      w = op(w, weight[x]);
-      x = parent_or_size[x];
-    }
-    while (a != x) {
+    int pre = -1;
+    while (parent_or_size[a] >= 0) {
       int na = parent_or_size[a];
-      S nw = op(inv(weight[a]), w);
-      parent_or_size[a] = x;
-      weight[a] = w;
+      parent_or_size[a] = pre;
+      pre = a;
       a = na;
-      w = nw;
     }
-    return x;
+    S w = e();
+    while (pre != -1) {
+      w = op(w, weight[pre]);
+      weight[pre] = w;
+      int npre = parent_or_size[pre];
+      parent_or_size[pre] = a;
+      pre = npre;
+    }
+    return a;
   }
 
   int size(int a) {
@@ -88,3 +89,82 @@ struct weighted_union_find {
   std::vector<int> parent_or_size;
   std::vector<S> weight;
 };
+
+
+
+
+// test code
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < int(n); i++)
+constexpr int SZ = 10;
+using S = array<int, 10>;
+S op(S g, S h) {
+  // g;h
+  S res;
+  rep(i, SZ) res[i] = h[g[i]];
+  return res;
+}
+S e() {
+  S res;
+  rep(i, SZ) res[i] = i;
+  return res;
+}
+S inv(S g) {
+  S res;
+  rep(i, SZ) res[g[i]] = i;
+  return res;
+}
+// generates random numbers
+std::default_random_engine gen(std::chrono::system_clock::now().time_since_epoch().count());
+using dist_type = std::uniform_int_distribution<>;
+using param_type = dist_type::param_type;
+dist_type dist;
+
+#include<atcoder/dsu>
+
+void test() {
+  dist.param(param_type(1, 100));
+  int n = dist(gen);
+  // actual data;
+  vector<S> A(n);
+  for(auto& g : A) {
+    rep(i, SZ) g[i] = i;
+    shuffle(g.begin(), g.end(), gen);
+  }
+  weighted_union_find<S, op, e, inv> d(n);
+  atcoder::dsu uf(n);
+  dist.param(param_type(20, 300));
+  int q = dist(gen);
+  int q0 = 0, q1 = 0;
+  rep(_, q) {
+    dist.param(param_type(0, 1));
+    int type = dist(gen);
+    dist.param(param_type(0, n - 1));
+    int u = dist(gen), v = dist(gen);
+    // calc weight u->v; gu^-1 gv
+    S w;
+    rep(i, SZ) w[A[u][i]] = A[v][i];
+    if (type == 0) {
+      q0++;
+      // check diff if connected
+      if (!uf.same(u, v)) continue;
+      S res = d.diff(u, v);
+      assert(res == w);
+    } else {
+      q1++;
+      // do merge if not connected
+      if (uf.same(u, v)) continue;
+      d.merge(u, v, w);
+      uf.merge(u, v);
+    }
+  }
+  cout << q << " queries successfully processed; ";
+  cout << q0 << ' ' << q1 << ' ' << d.groups().size() << endl;
+}
+
+int main(void) {
+  int tt;
+  cin >> tt;
+  while(tt--) test();
+}
