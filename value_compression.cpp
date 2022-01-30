a1b2dbd0c9c43f43e01b260ec466df92bf955874
_ <>
2022-01-30 18:35:26 +0900

    improve value compression

--- value_compression.cpp
+++ value_compression.cpp
@@ -1,38 +1,19 @@
 template<class S>
 struct value_compression : vector<S> {
-    value_compression(int rsv) { this->reserve(rsv); }
-    template<class T>
-    void add(const T& v) {
-        if constexpr (is_arithmetic_v<T>) this->push_back(v);
-        else for(auto&& x: v) this->push_back(x);
-    }
-    void build() {
-        sort(this->begin(), this->end());
-        this->erase(unique(this->begin(), this->end()), this->end());
-    }
-    template<class T>
-    int operator()(T x) { return lower_bound(this->begin(), this->end(), x) - this->begin(); }
-    template<class T>
-    void convert_iter(T& v) { for(auto& x: v) x = (*this)(x); }
+  bool built = false;
+  using VS = vector<S>;
+  using VS::vector;
+  value_compression(vector<S> v) : vector<S>(move(v)) {}
+  void build() {
+    sort(VS::begin(), VS::end());
+    VS::erase(unique(VS::begin(), VS::end()), VS::end());
+    built = true;
+  }
+  template<class T>
+  void convert(T first, T last) { for (; first != last; ++first) *first = (*this)(*first); }
+  int operator()(S x) {
+    assert(built);
+    return lower_bound(VS::begin(), VS::end(), x) - VS::begin();
+  }
+  void clear() { VS::clear(); built = false; }
 };
-
-
-
-// // example
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main() {
-//     int n;
-//     cin >> n;
-//     vector<int> a(n);
-//     for(int i = 0; i < n; i++) cin >> a[i];
-//     value_compression<int> d2i(n + 2);
-//     d2i.add(a);
-//     d2i.add(-1);
-//     d2i.add(1001001001);
-//     d2i.build();
-//     for(int x: a) cout << d2i(x) << ' ';
-//     d2i.convert_iter(a);
-//     for(int x: a) cout << x << ' ';
-//     cout << d2i.size() << endl;
-// }
