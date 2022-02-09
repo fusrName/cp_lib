4856b9389ffb385f1085183a571bdf0e8f94428f
_ <>
2022-02-10 02:55:38 +0900

    add root_cycles

--- cycle_decomposition.cpp
+++ cycle_decomposition.cpp
@@ -14,3 +14,31 @@ vector<vector<int>> cycle_decomposition(const vector<int>& a) {
   }
   return res;
 }
+
+vector<vector<int>> root_cycles(const vector<int>& a) {
+  int n = a.size();
+  vector<char> visited(n);
+  vector<char> in_stack(n);
+  vector<int> st;
+  vector<vector<int>> res;
+  for (int i = 0; i < n; i++) {
+    if (visited[i]) continue;
+    int v = i;
+    do {
+      visited[v] = true;
+      st.emplace_back(v);
+      in_stack[v] = true;
+      v = a[v];
+      if (in_stack[v]) {
+        auto it = find(st.begin(), st.end(), v);
+        res.emplace_back(it, st.end());
+        break;
+      }
+    } while (!visited[v]);
+    while (!st.empty()) {
+      in_stack[st.back()] = false;
+      st.pop_back();
+    }
+  }
+  return res;
+}
