1f9046885bb48cf7b0bdd576880a2f01de83e354
_ <>
2022-03-06 06:14:16 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
+namespace {
 using namespace std;
-#define rep(i,n) for(int i = 0; i < int(n); ++i)
-#define rrep(i,n) for(int i = int(n)-1; i >= 0; --i)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-template<class T> void chmax(T& a, const T& b) {a = max(a, b);}
-template<class T> void chmin(T& a, const T& b) {a = min(a, b);}
+#define rep(i,n) for(int i = 0; i < (int)(n); i++)
+#define rrep(i,n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+template<class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return true; } else return false; }
+template<class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return true; } else return false; }
 using ll = long long;
 using P = pair<int,int>;
 using VI = vector<int>;
@@ -13,7 +14,11 @@ using VVI = vector<VI>;
 using VL = vector<ll>;
 using VVL = vector<VL>;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+} int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while(tt--) {
+  }
 }
