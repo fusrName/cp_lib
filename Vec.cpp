0ad9986216375885e26f7affdc87d964df419fb0
_ <>
2022-03-06 05:28:53 +0900

    improve Vec

--- Vec.cpp
+++ Vec.cpp
@@ -1,71 +1,48 @@
 struct Vec {
-    long long x = 0, y = 0;
-
-    friend Vec operator+(const Vec& a, const Vec& b) {
-        return {a.x + b.x, a.y + b.y};
-    }
-    friend Vec operator-(const Vec& a, const Vec& b) {
-        return {a.x - b.x, a.y - b.y};
-    }
-    friend long long operator*(const Vec& a, const Vec& b) {
-        return a.x * b.y - a.y * b.x;
-    }
-    friend Vec operator*(long long c, const Vec& a) {
-        return {c * a.x, c * a.y};
-    }
-    friend Vec operator*(const Vec& a, long long c) {
-        return c * a;
-    }
-    friend Vec operator/(const Vec& a, long long c) {
-        return {a.x / c, a.y / c};
-    }
-    long long norm2() {
-        return x * x + y * y;
-    }
-    bool is_parallel(const Vec& a) {
-        // return __int128_t(x) * a.y == __int128_t(y) * a.x;
-        return (*this) * a == 0;
-    }
-    long long dot(const Vec& a) {
-        return x * a.x + y * a.y;
-    }
-    Vec& operator+=(const Vec& rhs) {
-        return *this = *this + rhs;
-    }
-    Vec& operator-=(const Vec& rhs) {
-        return *this = *this - rhs;
-    }
-    Vec& operator*=(long long c) {
-        x *= c;
-        y *= c;
-        return *this;
-    }
-    friend bool operator==(const Vec& lhs, const Vec& rhs) {
-        return lhs.x == rhs.x && lhs.y == rhs.y;
-    }
-    friend bool operator!=(const Vec& lhs, const Vec& rhs) {
-        return lhs.x != rhs.x || lhs.y != rhs.y;
-    }
-    friend bool operator<(const Vec& a, const Vec& b) {
-        if (a.x == 0 && a.y == 0) return !(b.x == 0 && b.y == 0);
-        if (b.x == 0 && b.y == 0) return false;
-        int qa = 0 < a.x && 0 <= a.y ? 0:
-                 a.x <= 0 && 0 < a.y ? 1:
-                 a.x < 0             ? 2:
-                                       3;
-        int qb = 0 < b.x && 0 <= b.y ? 0:
-                 b.x <= 0 && 0 < b.y ? 1:
-                 b.x < 0             ? 2:
-                                       3;
-        if (qa != qb) return qa < qb;
-        // auto t1 = __int128_t(a.x) * b.y, t2 = __int128_t(a.y) * b.x;
-        auto t1 = a.x * b.y, t2 = a.y * b.x;
-        return t1 > t2 || (t1 == t2 && (std::abs(a.x) < std::abs(b.x) || std::abs(a.y) < std::abs(b.y)));
-    }
-    friend std::ostream& operator<< (std::ostream& os, const Vec& t) {
-        return os << t.x << ' ' << t.y;
-    }
-    friend std::istream& operator>> (std::istream& os, Vec& t) {
-        return os >> t.x >> t.y;
-    }
+  using etype = long long;  // type of elements
+  using ptype = long long;  // type of product results
+  etype x = 0, y = 0;
+  constexpr Vec operator-() const { return {-x, -y}; }
+  friend constexpr Vec operator+(const Vec& a, const Vec& b) { return {a.x + b.x, a.y + b.y}; }
+  friend constexpr Vec operator-(const Vec& a, const Vec& b) { return {a.x - b.x, a.y - b.y}; }
+  friend constexpr etype operator*(const Vec& a, const Vec& b) { return a.x * b.y - a.y * b.x; }
+  template <class T, enable_if_t<is_integral_v<T>>* = nullptr>
+  friend constexpr Vec operator*(T c, const Vec& a) { return {c * a.x, c * a.y}; }
+  template <class T, enable_if_t<is_integral_v<T>>* = nullptr>
+  friend constexpr Vec operator*(const Vec& a, T c) { return c * a; }
+  template <class T, enable_if_t<is_integral_v<T>>* = nullptr>
+  friend constexpr Vec operator/(const Vec& a, T c) { return {a.x / c, a.y / c}; }
+  Vec& operator+=(const Vec& a) { x += a.x; y += a.y; return *this; }
+  Vec& operator-=(const Vec& a) { x -= a.x; y -= a.y; return *this; }
+  template <class T, enable_if_t<is_integral_v<T>>* = nullptr>
+  Vec& operator*=(T c) { x *= c; y *= c; return *this; }
+  friend constexpr bool operator==(const Vec& a, const Vec& b) { return a.x == b.x && a.y == b.y; }
+  friend constexpr bool operator!=(const Vec& a, const Vec& b) { return !(a == b); }
+  constexpr int quadrant() const { return y > 0 ? (x > 0 ? 0 : 1) : y < 0 ? (x < 0 ? 2 : 3) : (x > 0 ? 0 : x < 0 ? 2 : -1); }
+  friend constexpr bool operator<(const Vec& a, const Vec& b) {
+    int qa = a.quadrant(), qb = b.quadrant();
+    if (qa != qb) return qa < qb;
+    ptype p1 = (ptype)a.x * b.y, p2 = (ptype)a.y * b.x;
+    return p1 > p2 || (p1 == p2 && (a.x != 0 ? abs(a.x) < abs(b.x) : abs(a.y) < abs(b.y)));
+  }
+  friend constexpr bool operator>(const Vec& a, const Vec& b) { return b < a; }
+  friend constexpr bool operator<=(const Vec& a, const Vec& b) { return !(b < a); }
+  friend constexpr bool operator>=(const Vec& a, const Vec& b) { return !(a < b); }
+  constexpr bool parallel_to(const Vec& a) const { return (ptype)x * a.y == (ptype)y * a.x; }
+  constexpr ptype dot(const Vec& a) const { return (ptype)x * a.x + (ptype)y * a.y; }
+  constexpr ptype norm2() const { return (ptype)x * x + (ptype)y * y; }
+  constexpr bool is_zero() const { return x == 0 && y == 0; }
+  constexpr Vec normalize() const {
+    if (x == 0) return Vec{0, (y == 0 ? 0 : 1)};
+    etype g = gcd(x, y);
+    return *this / (x > 0 ? g : -g);
+  }
+  constexpr Vec rot90() const { return {-y, x}; }
+  // exclusive
+  constexpr bool in_between(const Vec& a, const Vec& b) const {
+    Vec x = a - *this, y = b - *this;
+    return x.parallel_to(y) && x.dot(y) < 0;
+  }
+  friend std::ostream& operator<< (std::ostream& os, const Vec& t) { return os << t.x << ' ' << t.y; }
+  friend std::istream& operator>> (std::istream& os, Vec& t) { return os >> t.x >> t.y; }
 };
