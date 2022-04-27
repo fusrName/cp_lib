41788a712a5c658101b307f469ec40f90a598281
_ <>
2022-04-27 17:50:59 +0900

    add range_edge

---
+++ range_edge.cpp
@@ -0,0 +1,54 @@
+struct range_edge {  // based on DST-like indexing
+  const int L, R, offset;
+  const vector<unsigned char> msb;
+
+  range_edge(int L, int R, int offset): L(L), R(R), offset(offset), msb(build_msb_table(R - L)) {
+    assert(L <= R);
+  }
+  range_edge(int R): range_edge(0, R, R) {}
+
+  vector<unsigned char> build_msb_table(int n) {
+    if (n <= 1) return {};
+    unsigned char k_max = 32 - __builtin_clz(n - 1);
+    vector<unsigned char> res(1 << k_max);
+    unsigned char* p = res.data();
+    for (unsigned char k = 0; k < k_max; k++) {
+      memset(p + (1U << k), k, 1U << k);
+    }
+    return res;
+  }
+  int size() {
+    int n = R - L;
+    if (n <= 1) return 0;
+    int k = 31 - __builtin_clz(n - 1);
+    return k * n;
+  }
+  vector<pair<int, int>> edges() {
+    const int n = R - L;
+    if (n <= 1) return {};
+    vector<pair<int, int>> res;
+    int k_max = 32 - __builtin_clz(n - 1);
+    for (int k = 1, os = offset; k < k_max; k++, os += n) {
+      for (int i = 0; i < n; i++) res.emplace_back(os + i, L + i);
+      for (int c = 1 << k; c < n; c += 1 << (k + 1)) {
+        int l = c - (1 << k);
+        int r = min(n, c + (1 << k));
+        for (int i = c - 2; i >= l; i--) res.emplace_back(os + i, os + i + 1);
+        for (int i = c + 1; i < r; i++) res.emplace_back(os + i, os + i - 1);
+      }
+    }
+    return res;
+  }
+  array<int, 2> query_closed(int l, int r) {
+    assert(L <= l && l <= r && r < R);
+    if (l == r) return {l, -1};
+    else if (l + 1 == r) return {l, r};
+    l -= L, r -= L;
+    int k = msb[l ^ r];
+    int os = offset + (R - L) * (k - 1);
+    return {os + l, os + r};
+  }
+  array<int, 2> query(int l, int r) {
+    return l == r ? array{-1, -1} : query_closed(l, r - 1);
+  }
+};
