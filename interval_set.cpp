365e3011d12554524c85ea3cce64a173d8ee24ac
_ <>
2022-05-15 14:37:01 +0900

    simplified interval_set

--- interval_set.cpp
+++ interval_set.cpp
@@ -3,66 +3,46 @@ using namespace std;
 
 template <class T>
 struct interval_set {
+  // mp[R] = L for each [L, R)
   map<T, T> mp{{numeric_limits<T>::min(), numeric_limits<T>::min()},
                {numeric_limits<T>::max(), numeric_limits<T>::max()}};
   T size = 0;
 
-  void insert(T l, T r) {
-    if (l == r) return;
-    assert(l < r);
-    auto it = mp.lower_bound(l);
-    if (it->first != l) --it;
-    T cur_right;  // [it->first, cur_right) will be expanded to right until reaching r
-    if (it->second < l) {
-      it = mp.emplace(l, 0).second;
-      cur_right = l;
-    } else {
-      if (r <= it->second) return;
-      cur_right = it->second;
-    }
-    // it->second gets updated before return
-    ++it;
-    while (true) {
-      if (r < it->first) {
-        size += r - cur_right;
-        it->second = r;
-        return;
-      }
-      size += it->first - cur_right;
-      cur_right = it->second;
+  void insert(T left, T right) {
+    if (left == right) return;
+    assert(left < right);
+    // remove all ranges [it->second, it->first) that overwrap with / neighbor on [left, right)
+    // i.e., left <= it->first && it->second <= right
+    auto it = mp.lower_bound(left);
+    while (it->second <= right) {
+      size -= it->first - it->second;
+      chmin(left, it->second);
+      chmax(right, it->first);
       it = mp.erase(it);
-      if (r <= cur_right) {
-        it->second = cur_right;
-        return;
-      }
     }
+    mp.emplace_hint(it, left, right);
+    size += right - left;
   }
-  void erase(T l, T r) {
-    if (l == r) return;
-    assert(l < r);
-    auto it = mp.lower_bound(l);
-    if (it->first != l) {
-      auto pit = prev(it);
-      if (l < pit->second) {
-        size -= pit->second - l;
-        pit->second = l;
-      }
+  void erase(T left, T right) {
+    if (left == right) return;
+    assert(left < right);
+    auto it = mp.upper_bound(left);
+    if (it->second < left) {
+      mp.emplace_hint(left, left, it->second);
+      size += it->left - it->second;
     }
-    while (true) {
-      if (r <= it->first) return;
-      if (r < it->second) {
-        size -= r - it->first;
-        mp.emplace_hint(mp.erase(it), r, it->second);
+    while (it->second < right) {
+      if (it->first <= right) {
+        size -= it->first - it->second;
+        it = mp.erase(it);
+      } else {
+        size -= right - it->second;
+        it->second = right;
         return;
       }
-      size -= it->second - it->first;
-      it = mp.erase(it);
     }
   }
   bool contains(T x) {
-    auto it = lower_bound(x);
-    if (it->first == x) return true;
-    --it;
-    return x < it->second;
+    return mp.upper_bound(x)->second <= x;
   }
 };
