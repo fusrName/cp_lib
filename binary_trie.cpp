ee398392b4050bbe7bbf29a25bd4538157ec9b0c
_ <>
2022-05-21 04:39:01 +0900

    fix erase, extract, and merge in

--- binary_trie.cpp
+++ binary_trie.cpp
@@ -44,26 +44,33 @@ struct binary_trie {
       nodes[p].count++;
     }
   }
-  bool erase(T x) {
-    if (root == -1) return false;
+  int erase(T x, int num = 1) {
+    assert(num >= 0);
+    if (root == -1) return 0;
     int idx[bit_length + 1];
     int p = root;
     idx[bit_length] = p;
     for (int k = bit_length - 1; k >= 0; k--) {
       p = nodes[p].ch[x >> k & 1];
-      if (p == -1) return false;
+      if (p == -1) return 0;
       idx[k] = p;
     }
+    if (num < nodes[p].count) {
+      nodes[p].count -= num;
+      return num;
+    }
+    num = nodes[p].count;
     int k = 0;
-    while (k <= bit_lengh && node[idx[k]].count == 1) {
+    while (k <= bit_lengh && node[idx[k]].count == num) {
       nodes.release(idx[k]);
       k++;
     }
     if (k == bit_length + 1) root = -1;
     else {
-      if (k) nodes[idx[k]].ch[x >> (k - 1) & 1] = -1;
-      for (; k <= bit_length; k++) nodes[idx[k]].count--;
+      nodes[idx[k]].ch[x >> (k - 1) & 1] = -1;
+      for (; k <= bit_length; k++) nodes[idx[k]].count -= num;
     }
+    return num;
   }
   T xor_min(T x) {
     assert(root != -1);
@@ -101,49 +108,62 @@ struct binary_trie {
     }
     return nodes[p].count;
   }
+  bool empty() { return root == -1; }
   int size() { return root == -1 ? 0 : nodes[root].count; }
   binary_trie extract_first(int num) {
-    assert(0 <= num && num <= size());
+    int sz = size();
+    assert(0 <= num && num <= sz);
     binary_trie res;
-    if (nodes[root].count == size) {
-      swap(nodes[root], nodes[res.root]);
+    if (num == 0) return res;
+    else if (num == sz) {
+      swap(root, res.root);
       return res;
-    } else if (size == 0) return res;
+    }
+    res.root = nodes.create();
     int p_from = root;
     int p_to = res.root;
-    while (true) {
-      nodes[p_from].count -= size;
-      nodes[p_to].count = size;
-      int nxt0 = nodes[p_from].ch[0];
-      if (nxt0 == -1) {
-        if (nodes[p_from].ch[1] == -1) break;
-        p_from = nodes[p_from].ch[1];
-        p_to = nodes[p_to].ch[1] = nodes.create();
-      } else {
-        if (size < nodes[nxt0].count) {
-          p_from = nxt0;
+    nodes[p_from] -= num;
+    nodes[p_to] = num;
+    for (int k = bit_length - 1; k >= 0; k--) {
+      // loop invariant: 0 < num && num < nodes[p_from].count
+      int c0 = nodes[p_from].ch[0];
+      if (c0 != -1) {
+        int csz = nodes[c0].count;
+        if (csz > num) {
+          p_from = c0;
           p_to = nodes[p_to].ch[0] = nodes.create();
+          nodes[p_from].count -= num;
+          nodes[p_to].count = num;
+          continue;
         } else {
-          nodes[p_to].ch[0] = nxt0;
           nodes[p_from].ch[0] = -1;
-          size -= nodes[nxt0].count;
-          if (size == 0) break;
-          p_from = nodes[p_from].ch[1];
-          p_to = nodes[p_to].ch[1] = nodes.create();
+          nodes[p_to].ch[0] = c0;
+          num -= csz;
+          if (num == 0) break;
         }
       }
+      p_from = nodes[p_from].ch[1];
+      p_to = nodes[p_to].ch[1] = nodes.create();
+      nodes[p_from].count -= num;
+      nodes[p_to].count = num;
     }
     return res;
   }
-  binary_trie extract_last(int size) {
-    assert(0 <= size && size <= nodes[root].count);
-    binary_trie res = extract_first(nodes[root].count - size);
+  binary_trie extract_last(int num) {
+    int sz = size();
+    binary_trie res = extract_first(sz - num);
     swap(res, *this);
     return res;
   }
   void merge_in(binary_trie&& bt) {
     static vector<pair<int, int>> todo;
-    todo.emplace_back(root, bt.root);
+    binary_trie bt_local = move(bt);
+    if (bt_local.empty()) return;
+    if (empty()) {
+      *this = move(bt_local);
+      return;
+    }
+    todo.emplace_back(root, bt_local.root);
     do {
       auto [p, q] = todo.back(); todo.pop_back();
       nodes[p].count += nodes[q].count;
