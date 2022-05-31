b9bd1a3b8253d67819a573a42441e08b72be71ce
_ <>
2022-06-01 05:14:42 +0900

    add cartesian tree with edges parent->children

--- cartesian_tree.cpp
+++ cartesian_tree.cpp
@@ -1,70 +1,37 @@
-template<class T>
-std::vector<int> max_heap_cartesian_tree(const std::vector<T>& a) {
-    const unsigned int n = a.size();
-    std::vector<int> p(n, -1);
-    std::stack<int> s;
-    for(unsigned int i = 0; i < n; i++) {
-        int last_popped = -1;
-        while(!s.empty() && a[s.top()] <= a[i]) {
-            last_popped = s.top(); s.pop();
-        }
-        if (!s.empty()) p[i] = s.top();
-        s.push(i);
-        if (last_popped != -1) p[last_popped] = i;
+// At root is the minimum by default
+template<class T, class Compare=less<T>>
+vector<int> cartesian_tree(const vector<T>& a) {
+  const int n = a.size();
+  vector<int> p(n, -1);
+  vector<int> st;
+  constexpr Compare cmp;
+  for (int i = 0; i < n; i++) {
+    int last_popped = -1;
+    // Left one wins on tie if Compare = less/greater
+    while (!st.empty() && cmp(a[i], a[st.back()])) {
+      last_popped = st.back(); st.pop_back();
     }
-    return p;
+    if (last_popped != -1) p[last_popped] = i;
+    if (!st.empty()) p[i] = st.back();
+    st.emplace_back(i);
+  }
+  return p;
 }
 
-template<class T>
-std::vector<int> min_heap_cartesian_tree(const std::vector<T>& a) {
-    const unsigned int n = a.size();
-    std::vector<int> p(n, -1);
-    std::stack<int> s;
-    for(unsigned int i = 0; i < n; i++) {
-        int last_popped = -1;
-        while(!s.empty() && a[s.top()] >= a[i]) {
-            last_popped = s.top(); s.pop();
-        }
-        if (!s.empty()) p[i] = s.top();
-        s.push(i);
-        if (last_popped != -1) p[last_popped] = i;
+template<class T, class Compare=less<T>>
+pair<vector<array<int, 2>>, int> cartesian_tree(const vector<T>& a) {
+  const int n = a.size();
+  vector<array<int, 2>> g(n, {-1, -1});
+  vector<int> st;
+  constexpr Compare cmp;
+  for (int i = 0; i < n; i++) {
+    int last_popped = -1;
+    while (!st.empty() && cmp(a[i], a[st.back()])) {
+      last_popped = st.back(); st.pop_back();
     }
-    return p;
-}
-
-std::vector<std::vector<int>> convert_p2c(const std::vector<int>& parents) {
-    const unsigned int n = parents.size();
-    std::vector<std::vector<int>> children(n);
-    for(int i = 0; i < n; i++) children[parents[i]].push_back(i);
-    return children
-}
-
-
-template<class T>
-std::tuple<std::vector<int>, std::vector<int>, std::vector<int>> max_heap_cartesian_tree(const std::vector<T>& a) {
-    const unsigned int n = a.size();
-    std::tuple<std::vector<int>, std::vector<int>, std::vector<int>> res;
-    auto& p = std::get<0>(res);
-    auto& l = std::get<1>(res);
-    auto& r = std::get<2>(res);
-    p.resize(n, -1);
-    l.resize(n, -1);
-    r.resize(n, -1);
-    std::stack<int> s;
-    for(unsigned int i = 0; i < n; i++) {
-        int last_popped = -1;
-        while(!s.empty() && a[s.top()] <= a[i]) {
-            last_popped = s.top(); s.pop();
-        }
-        if (!s.empty()) {
-            r[s.top()] = i;
-            p[i] = s.top();
-        }
-        s.push(i);
-        if (last_popped != -1) {
-            l[i] = last_popped;
-            p[last_popped] = i;
-        }
-    }
-    return res;
+    g[i][0] = last_popped;
+    if (!st.empty()) g[st.back()][1] = i;
+    st.emplace_back(i);
+  }
+  return pair(move(g), st.empty() ? -1 : st[0]);
 }
