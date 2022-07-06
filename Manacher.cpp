f8d3f867ef649ba3bb7a8cd2dfc70ad4ae137839
_ <>
2022-07-07 05:39:18 +0900

    add Manacher for cursor ver.

--- Manacher.cpp
+++ Manacher.cpp
@@ -12,3 +12,23 @@ vector<int> Manacher(const T& s) {
   }
   return res;
 }
+
+template <class T>
+vector<int> Manacher_cursor(const T& s) {
+  const int n = s.size();
+  vector<int> res(n + 1);
+  for (int i = 1, r = 1; i < n;) {
+    int l = 2 * i - r;
+    while (l && r < n && s[l - 1] == s[r]) l--, r++;
+    int radius = r - i;
+    res[i] = radius;
+    if (radius == 0) {
+      i = r = i + 1;
+    } else {
+      int j = i - 1;
+      i++;
+      while (i + res[j] < r) res[i++] = res[j--];
+    }
+  }
+  return res;
+}
