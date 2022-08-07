94398d1b81bb3c8e5d824776d75a4bab061b8579
_ <>
2022-08-07 12:23:06 +0900

    refactor kBFS

--- graph/kBFS.cpp
+++ graph/kBFS.cpp
@@ -1,32 +1,36 @@
 constexpr int INF = 1001001001;
-template <class T, int k = 2>
+template <int k = 2, class T>  // cost in [0, k)
 vector<int> kBFS(const vector<vector<pair<int, T>>>& g, int s = 0) {
+  static_assert(k >= 1);
   const int n = g.size();
   vector<int> dist(n, INF);
-  deque<vector<int>> q(k);
+  vector<int> q[2 * k];
   dist[s] = 0;
   q[0].emplace_back(s);
-  for (int dist_cur = 0;;) {
-    vector<int>& vs = q[0];
-    while (!vs.empty()) {
-      int u = vs.back();
-      vs.pop_back();
+  for (int dist_cur = 0, ptr = 0;;) {
+    const auto qb = q + ptr;
+    while (!qb[0].empty()) {
+      int u = qb[0].back();
+      qb[0].pop_back();
       if (dist[u] != dist_cur) continue;
       for (auto [v, c] : g[u]) {
         int nd = dist_cur + c;
         if (nd < dist[v]) {
           dist[v] = nd;
-          q[c].emplace_back(v);
+          qb[c].emplace_back(v);
         }
       }
     }
-    for(int i = 1;; i++) {
+    do {
       dist_cur++;
-      vector<int> tmp = move(q.front());
-      q.pop_front();
-      q.emplace_back(move(tmp));
-      if (!q[0].empty()) break;
-      if (i == k - 1) return dist;
+      ptr++;
+      if (ptr == 2 * k) return dist;
+    } while (q[ptr].empty());
+    if (ptr > k) {
+      for (int i = ptr; i < 2 * k; i++) {
+        swap(q[i], q[i - ptr]);
+      }
+      ptr = 0;
     }
   }
 }
