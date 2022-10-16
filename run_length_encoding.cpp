1e931d533977fc6e00e09f02aed5a00c4f09a5c6
_ <>
2022-10-16 13:42:23 +0900

    turn run length encoding into class

--- run_length_encoding.cpp
+++ run_length_encoding.cpp
@@ -1,32 +1,41 @@
-// returns a vector of (val, len)
-template <class Iter>
-auto run_length_encoding(Iter first, Iter last) {
-  vector<pair<typename iterator_traits<Iter>::value_type, int>> res;
-  if (first != last) {
-    auto v = *first++;
-    int cnt = 1;
-    for (; first != last; first++) {
-      if (*first == v) cnt++;
-      else {
-        res.emplace_back(v, cnt);
-        v = *first;
-        cnt = 1;
+template <class T, class C = long long>
+struct run_length_encoding {
+  vector<pair<T, C>> d;
+
+  run_length_encoding() {}
+  template <class Iter>
+  run_length_encoding(Iter first, Iter last) {
+    while (first != last) {
+      auto v = *first++;
+      int cnt = 1;
+      while (first != last && *first == v) {
+        first++;
+        cnt++;
       }
+      d.emplace_back(v, cnt);
     }
-    res.emplace_back(v, cnt);
   }
-  return res;
-}
-
-
-template <class T>
-void rle_push_back(vector<pair<T, int>>& a, T v, int len=1) {
-  if (!a.empty() && a.back().first == v) a.back().second += len;
-  else a.emplace_back(v, len);
-}
-
-template <class T>
-void rle_push_back(vector<pair<T, int>>& a, pair<T, int> vl) {
-  if (!a.empty() && a.back().first == vl.first) a.back().second += vl.second;
-  else a.emplace_back(vl);
-}
+  void push_back(T v, C count) {
+    assert(count >= 0);
+    if (count == 0) return;
+    if (!d.empty() && d.back().first == v) {
+      d.back().second += count;
+    } else {
+      d.emplace_back(v, count);
+    }
+  }
+  bool operator<(const run_length_encoding& rhs) const {
+    int i = 0;
+    int ub = min(d.size(), rhs.d.size());
+    while (i < ub && d[i] == rhs.d[i]) i++;
+    if (i == ub) return d.size() < rhs.d.size();
+    auto [vl, cl] = d[i];
+    auto [vr, cr] = rhs.d[i];
+    if (vl != vr) return vl < vr;
+    if (cl < cr) {
+      return i + 1 == int(d.size()) || d[i + 1].first < vr;
+    } else {
+      return i + 1 < int(rhs.d.size()) && vl < rhs.d[i + 1].first;
+    }
+  }
+};
