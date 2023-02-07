e50679029dcd716a273eafc25906e5655d6013ef
_ <>
2023-02-07 15:39:56 +0900

    refactor methods above inv_proc

--- myFPS.cpp
+++ myFPS.cpp
@@ -3,137 +3,94 @@ struct FPS : vector<T> {
   using vector<T>::vector;
 
   FPS& operator+=(const FPS& f) {
-    int tsz = (*this).size(), fsz = f.size();
-    if (tsz < fsz) {
-      (*this).insert((*this).end(), f.begin() + tsz, f.end());
-      while (tsz--) (*this)[tsz] += f[tsz];
-    } else {
-      while (fsz--) (*this)[fsz] += f[fsz];
+    const int tsz = this->size(), fsz = f.size();
+    const int csz = min(tsz, fsz);
+    for (int i = 0; i < csz; i++) (*this)[i] += f[i];
+    if (csz < fsz) {
+      this->insert(this->end(), f.begin() + csz, f.end());
     }
     return *this;
   }
   FPS& operator+=(FPS&& f) {
-    int tsz = (*this).size(), fsz = f.size();
-    if (tsz < fsz) {
-      swap(*this, f);
-      while (tsz--) (*this)[tsz] += f[tsz];
-    } else {
-      while (fsz--) (*this)[fsz] += f[fsz];
-    }
+    if (this->size() < f.size()) swap(*this, f);
+    *this += f;
     return *this;
   }
   friend FPS operator+(const FPS& f, const FPS& g) {
-    int fsz = f.size(), gsz = g.size();
     FPS h;
-    if (fsz < gsz) {
-      h.reserve(gsz);
-      for (int i = 0; i < fsz; i++) h.emplace_back(f[i] + g[i]);
-      h.insert(h.end(), g.begin() + fsz, g.end());
-    } else {
-      h.reserve(fsz);
-      for (int i = 0; i < gsz; i++) h.emplace_back(f[i] + g[i]);
-      h.insert(h.end(), f.begin() + gsz, f.end());
-    }
+    h.reserve(max(f.size(), g.size()));
+    h = f;
+    h += g;
     return h;
   }
-  friend FPS operator+(FPS&& f, const FPS& g) {
-    f += g;
-    return move(f);
-  }
-  friend FPS operator+(const FPS& f, FPS&& g) {
-    g += f;
-    return move(g);
-  }
-  friend FPS operator+(FPS&& f, FPS&& g) {
-    f += move(g);
-    return move(f);
-  }
+  friend FPS operator+(FPS&& f, const FPS& g) { return move(f += g); }
+  friend FPS operator+(const FPS& f, FPS&& g) { return move(g += f); }
+  friend FPS operator+(FPS&& f, FPS&& g) { return move(f += move(g)); }
 
   FPS& operator-=(const FPS& f) {
-    int tsz = (*this).size(), fsz = f.size();
-    if (tsz < fsz) {
-      (*this).reserve(fsz);
-      for (int i = tsz; i < fsz; i++) {
-        (*this).emplace_back(-f[i]);
-      }
-      while (tsz--) (*this)[tsz] -= f[tsz];
-    } else {
-      while (fsz--) (*this)[fsz] -= f[fsz];
+    const int tsz = this->size(), fsz = f.size();
+    const int csz = min(tsz, fsz);
+    for (int i = 0; i < csz; i++) (*this)[i] -= f[i];
+    if (csz < fsz) {
+      this->resize(fsz);
+      for (int i = csz; i < fsz; i++) (*this)[i] = -f[i];
     }
     return *this;
   }
   FPS& operator-=(FPS&& f) {
-    int tsz = (*this).size(), fsz = f.size();
-    if (tsz < fsz) {
-      swap(*this, f);
-      while (fsz-- != tsz) (*this)[fsz] = -(*this)[fsz];
-      while (tsz--) (*this)[tsz] = f[tsz] - (*this)[tsz];
+    const int tsz = this->size(), fsz = f.size();
+    if (tsz > fsz) {
+      *this -= f;
     } else {
-      while (fsz--) (*this)[fsz] -= f[fsz];
+      for (int i = 0; i < tsz; i++) f[i] = (*this)[i] - f[i];
+      for (int i = tsz; i < fsz; i++) f[i] = -f[i];
+      swap(*this, f);
     }
     return *this;
   }
   friend FPS operator-(const FPS& f, const FPS& g) {
-    int fsz = f.size(), gsz = g.size();
     FPS h;
-    if (fsz < gsz) {
-      h.reserve(gsz);
-      int i = 0;
-      for (; i < fsz; i++) h.emplace_back(f[i] - g[i]);
-      for (; i < gsz; i++) h.emplace_back(-g[i]);
-    } else {
-      h.reserve(fsz);
-      for (int i = 0; i < gsz; i++) h.emplace_back(f[i] - g[i]);
-      h.insert(h.end(), f.begin() + gsz, f.end());
-    }
+    h.reserve(max(f.size(), g.size()));
+    h = f;
+    h -= g;
     return h;
   }
-  friend FPS operator-(FPS&& f, const FPS& g) {
-    f -= g;
-    return move(f);
-  }
+  friend FPS operator-(FPS&& f, const FPS& g) { return move(f -= g); }
   friend FPS operator-(const FPS& f, FPS&& g) {
-    int fsz = f.size(), gsz = g.size();
+    const int fsz = f.size(), gsz = g.size();
+    const int csz = min(fsz, gsz);
+    for (int i = 0; i < csz; i++) g[i] = f[i] - g[i];
     if (fsz < gsz) {
-      while (gsz-- != fsz) g[gsz] = -g[gsz];
-      while (fsz--) g[fsz] = f[fsz] - g[fsz];
+      for (int i = csz; i < gsz; i++) g[i] = -g[i];
     } else {
-      g.insert(g.end(), f.begin() + gsz, f.end());
-      while (gsz--) g[gsz] = f[gsz] - g[gsz];
+      g.insert(g.end(), f.begin() + csz, f.end());
     }
     return move(g);
   }
-  friend FPS operator-(FPS&& f, FPS&& g) {
-    f -= move(g);
-    return move(f);
-  }
+  friend FPS operator-(FPS&& f, FPS&& g) { return move(f -= move(g)); }
+  FPS operator+() && { return move(*this); }
+  FPS operator+() const& { return *this; }
   FPS operator-() && {
     for (T& x: *this) x = -x;
     return move(*this);
   }
   FPS operator-() const& {
     FPS f;
-    f.reserve((*this).size());
+    f.reserve(this->size());
     for (const T& x: *this) f.emplace_back(-x);
     return f;
   }
 
   static void multiply_naive(FPS& f, const FPS& g) {
     int n = int(f.size()), m = int(g.size());
-    if (!n || !m) {
-      f.clear();
-    } else if (n < m) {
-      FPS f_;
-      swap(tmp, f_);
-      if (f_.capacity() < f.capacity()) swap(f_, f);
-      else f_ = f;
+    if (n < m) {
+      tmp1 = f;
       f.assign(n + m - 1, T());
       for (int j = m - 1; j >= 0; j--) {
         for (int i = n - 1; i >= 0; i--) {
-          f[i + j] += f_[i] * g[j];
+          f[i + j] += tmp1[i] * g[j];
         }
       }
-      swap(tmp, f_);
     } else {
       f.resize(n + m - 1);
       for (int i = n - 1; i >= 0; i--) {
@@ -144,93 +101,32 @@ struct FPS : vector<T> {
       }
     }
   }
-  static void multiply_naive(FPS& f, FPS&& g) {
-    int n = int(f.size()), m = int(g.size());
-    if (n < m) swap(f, g);
-    if (int(f.capacity()) >= n + m - 1 || int(g.capacity()) < n + m - 1) {
-      multiply_naive(f, g);
-    } else {
-      multiply_naive(g, f);
-      swap(f, g);
-    }
-  }
 
-  static void multiply_fft(FPS& f_, const FPS& g_) {
-    int n = int(f_.size()), m = int(g_.size());
-    if (!n || !m) {
-      f_.clear();
-      return;
-    }
+  static void multiply_fft(FPS& f, const FPS& g) {
     int z = 1 << internal::ceil_pow2(n + m - 1);
-    FPS f, g;
-    swap(f, f_);
-    swap(g, tmp);
-    g.clear();
-    g.reserve(z);
-    g = g_;
-
-    f.resize(z);
-    internal::butterfly(f);
-    g.resize(z);
-    internal::butterfly(g);
-    for (int i = 0; i < z; i++) {
-      f[i] *= g[i];
-    }
+    tmp1.reserve(z), tmp1 = g, tmp1.resize(z), internal::butterfly(tmp1);
+    f.resize(z), internal::butterfly(f);
+    for (int i = 0; i < z; i++) f[i] *= g[i];
     internal::butterfly_inv(f);
     f.resize(n + m - 1);
     mint iz = mint(z).inv();
     for (int i = 0; i < n + m - 1; i++) f[i] *= iz;
-
-    swap(g, tmp);
-    swap(f, f_);
-    return;
-  }
-  static void multiply_fft(FPS& f_, FPS&& g_) {
-    int n = int(f_.size()), m = int(g_.size());
-    if (!n || !m) {
-      f_.clear();
-      return;
-    }
-    int z = 1 << internal::ceil_pow2(n + m - 1);
-    if (int(g_.capacity()) < z) {
-      multiply_fft(f_, g_);
-      return;
-    }
-    if (int(f_.capacity()) < z) {
-      multiply_fft(g_, f_);
-      swap(f_, g_);
-      return;
-    }
-    FPS f, g = move(g_);
-    swap(f, f_);
-
-    f.resize(z);
-    internal::butterfly(f);
-    g.resize(z);
-    internal::butterfly(g);
-    for (int i = 0; i < z; i++) {
-      f[i] *= g[i];
-    }
-    internal::butterfly_inv(f);
-    f.resize(n + m - 1);
-    mint iz = mint(z).inv();
-    for (int i = 0; i < n + m - 1; i++) f[i] *= iz;
-
-    swap(f, f_);
-    return;
   }
 
   FPS& operator*=(const FPS& f) {
     int n = int((*this).size()), m = int(f.size());
+    if (!n || !m) {
+      this->clear();
+      return *this;
+    };
     if (min(n, m) <= 60) multiply_naive(*this, f);
     else multiply_fft(*this, f);
     return *this;
   }
   FPS& operator*=(FPS&& f) {
     int n = int((*this).size()), m = int(f.size());
-    if (min(n, m) <= 60) multiply_naive(*this, move(f));
-    else multiply_fft(*this, move(f));
-    return *this;
+    if (this->size() < f.size()) swap(*this, f);
+    return *this *= f;
   }
   friend FPS operator*(FPS&& f, FPS&& g) { return move(f *= move(g)); }
   friend FPS operator*(FPS&& f, const FPS& g) { return move(f *= g); }
@@ -258,7 +154,7 @@ struct FPS : vector<T> {
     f += x;
     return f;
   }
-  FPS operator-=(T x) {
+  FPS& operator-=(T x) {
     if ((*this).empty()) (*this).emplace_back(-x);
     else (*this)[0] -= x;
     return *this;
@@ -295,23 +191,38 @@ struct FPS : vector<T> {
   }
   FPS& operator<<=(int x) {
     assert(x >= 0);
-    (*this).insert((*this).begin(), x, T());
+    this->insert((*this).begin(), x, T());
     return *this;
   }
-  friend FPS operator<<(FPS f, int x) {
+  friend FPS operator<<(const FPS& f, int x) {
+    assert(x >= 0);
+    const int fsz = f.size();
+    FPS res;
+    res.reserve(fsz + x);
+    res.resize(x);
+    res.insert(res.end(), f.begin(), f.end());
+    return res;
+  }
+  friend FPS operator<<(FPS&& f, int x) {
     f <<= x;
-    return f;
+    return move(f);
   }
   FPS& operator>>=(int x) {
     assert(x >= 0);
-    int tsz = (*this).size();
-    if (tsz <= x) (*this).clear();
-    else (*this).erase((*this).begin(), (*this).begin() + x);
+    const int tsz = this->size();
+    if (x >= tsz) this->clear();
+    else this->erase(this->begin(), this->begin() + x);
     return *this;
   }
-  friend FPS operator>>(FPS f, int x) {
+  friend FPS operator>>(const FPS& f, int x) {
+    assert(x >= 0);
+    const int fsz = f.size();
+    if (x >= fsz) return {};
+    else return FPS(f.begin() + x, f.end());
+  }
+  friend FPS operator>>(FPS&& f, int x) {
     f >>= x;
-    return f;
+    return move(f);
   }
 
   static FPS inv_proc(const FPS& f, FPS g, int len) {
