5b044987d50bcd7ebb9398238481a10ccf4a2e77
_ <>
2023-06-18 03:59:35 +0900

    replace queue with queue impl. by vector

--- graph/dist_bfs.cpp
+++ graph/dist_bfs.cpp
@@ -1,16 +1,17 @@
 vector<int> bfs(const vector<vector<int>>& g, int s=0) {
-    const int n = g.size();
-    vector<int> dist(n, -1);
-    queue<int> q;
-    dist[s] = 0;
-    q.push(s);
-    while(!q.empty()) {
-        int u = q.front(); q.pop();
-        int nd = dist[u] + 1;
-        for(int v: g[u]) if (dist[v] == -1) {
-            dist[v] = nd;
-            q.push(v);
-        }
+  const int n = g.size();
+  vector<int> dist(n, -1);
+  vector<int> q(n);
+  q[s] = 0;
+  dist[s] = 0;
+  int l = 0, r = 1;
+  while(l < r) {
+    int u = q[l++];
+    int nd = dist[u] + 1;
+    for(int v: g[u]) if (dist[v] == -1) {
+      dist[v] = nd;
+      q[r++] = v;
     }
-    return dist;
+  }
+  return dist;
 }
