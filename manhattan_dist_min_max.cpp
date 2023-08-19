45195ccbbaa2d77651b83f03981c15656e635b5f
_ <>
2023-08-19 20:21:50 +0900

    add manhattan_dist_min_max

--- manhattan_dist_min_max.cpp
+++ manhattan_dist_min_max.cpp
@@ -1,5 +1,5 @@
 template<class T>
-vector<T> manhattan_dist_max(vector<pair<T, T>> ps) {
+vector<T> manhattan_dist_max(const vector<pair<T, T>>& ps) {
   T v1, v2, v3, v4;
   v1 = v3 = numeric_limits<T>::max();
   v2 = v4 = numeric_limits<T>::min();
@@ -11,86 +11,112 @@ vector<T> manhattan_dist_max(vector<pair<T, T>> ps) {
     chmin(v3, t);
     chmax(v4, t);
   }
-  int n = ps.size();
-  vector<T> res;
-  res.reserve(n);
+  vector<T> res(ps.size());
+  int i = 0;
   for(auto [x, y] : ps) {
     T s = x + y;
     T t = -x + y;
-    res.emplace_back(max({s - v1, v2 - s, t - v3, v4 - t}));
+    res[i++] = max({s - v1, v2 - s, t - v3, v4 - t});
   }
   return res;
 }
+
+template<class S, auto e, auto op, bool dual=false, bool cursor_indexing=true>
+struct BIT {
+  vector<S> d;
+  int n;
+  template<class... Args>
+  BIT(auto&&... args) : d(forward<decltype(args)>(args)...), n(d.size()) {
+    if constexpr (dual) reverse(d.begin(), d.end());
+    for (int i = 1; i < n; i++) {
+      int j = i + (i & -i);
+      if (j < n) d[j] = op(d[j], d[i]);
+    }
+  }
+  BIT(int n) : d(n, e()), n(n) {}
+  void apply(int i, S x) {
+    if constexpr (cursor_indexing) {
+      assert(0 <= i && i <= n);
+      if constexpr (dual) i = n - i;
+    } else {
+      assert(0 <= i && i < n);
+      if constexpr (dual) i = n - 1 - i;
+    }
+    if (i == 0) {
+      if (n > 0) d[i] = op(d[i], x);
+    } else {
+      while (i < n) {
+        d[i] = op(d[i], x);
+        i += i & -i;
+      }
+    }
+  }
+  S sum(int i) {
+    if constexpr (cursor_indexing) {
+      assert(0 <= i && i <= n);
+      if constexpr (dual) i = n - i;
+      if (i == 0) return e();
+      i--;
+    } else {
+      assert(0 <= i && i < n);
+      if constexpr (dual) i = n - 1 - i;
+    }
+    S res = d[i];
+    while (i) {
+      i -= i & -i;
+      res = op(res, d[i]);
+    }
+    return res;
+  }
+};
 
 template<class T>
 vector<T> manhattan_dist_min(const vector<pair<T, T>>& points) {
   const int n = points.size();
   assert(n >= 2);
   struct S {
-    T x, y;
+    pair<T, T> p;
     int i;
   };
   vector<S> ps(n);
-  for (int i = 0; i < n; i++) {
-    auto [x, y] = points[i];
-    ps[i] = {x + y, -x + y, i};
-  }
+  for (int i = 0; i < n; i++) ps[i] = {points[i], i};
   sort(ps.begin(), ps.end(), [](const S& p1, const S& p2) {
-    return p1.x < p2.x;
+    return p1.p.first < p2.p.first;
   });
   vector<T> vals(n);
-  for (int i = 0; i < n; i++) vals[i] = ps[i].y;
+  for (int i = 0; i < n; i++) vals[i] = ps[i].p.second;
   sort(vals.begin(), vals.end());
   vals.erase(unique(vals.begin(), vals.end()), vals.end());
   const int sz = vals.size();
-  vector<int> ys(n);
-  for (int i = 0; i < n; i++) {
-    ys[i] = lower_bound(vals.begin(), vals.end(), ps[i].y) - vals.begin();
-  }
 
-  vector<T> ls(n, -1), rs(n);
-  auto dist = [&](const S& p1, const S& p2) {
-    return max(abs(p1.x - p2.x), abs(p1.y - p2.y));
-  };
-  for (int i = 1; i < n; i++) rs[0] = rs[i] = dist(ps[0], ps[i]);
-  struct E {
-    T x;
-    bool t;
-    int i;
-    bool operator<(const E& rhs) const {
-      return x < rhs.x || (x == rhs.x && t < rhs.t);
-    }
-  };
-  vector<E> evs;
-  fenwick_tree<int> ft(sz);
-  vector<int> cnt(n), yl(n), yr(n);
-  while(true) {
-    evs.clear();
-    for (int i = 0; i < n; i++) if (rs[i] - ls[i] > 1) {
-      T c = (rs[i] + ls[i]) / 2;
-      assert(c >= 0);
-      yl[i] = lower_bound(vals.begin(), vals.end(), ps[i].y - c) - vals.begin();
-      yr[i] = upper_bound(vals.begin(), vals.end(), ps[i].y + c) - vals.begin();
-      evs.push_back(E{ps[i].x - c, false, i});
-      evs.push_back(E{ps[i].x + c, true, i});
-    }
-    if (evs.empty()) break;
-    sort(evs.begin(), evs.end());
-    int ptr = 0;
-    for (auto [x, t, i] : evs) {
-      while(ptr < n && ((!t && ps[ptr].x < x) || (t && ps[ptr].x <= x))) {
-        ft.add(ys[ptr++], 1);
-      }
-      int sm = ft.sum(yl[i], yr[i]);
-      if (!t) {
-        cnt[i] = sm;
-      } else {
-        assert(cnt[i] + 1 <= sm);
-        (cnt[i] + 1 == sm ? ls[i] : rs[i]) = (ls[i] + rs[i]) / 2;
-      }
-    }
+  constexpr T inf_min = numeric_limits<T>::min() / 2;
+  constexpr T inf_max = numeric_limits<T>::max() / 2;
+  vector<T> ans(n, inf_max);
+  vector<int> y_id(n);
+  auto e = []{ return inf_min; };
+  auto op = [](T x, T y) { return max(x, y); };
+  BIT<T, e, op, false, false> bt1(sz);
+  BIT<T, e, op, true, false> bt2(sz);
+  for (int i = 0; i < n; i++) {
+    auto [x, y] = ps[i].p;
+    y_id[i] = lower_bound(vals.begin(), vals.end(), y) - vals.begin();
+    chmin(ans[ps[i].i], min(
+      x + y - bt1.sum(y_id[i]),
+      x - y - bt2.sum(y_id[i])
+    ));
+    bt1.apply(y_id[i], x + y);
+    bt2.apply(y_id[i], x - y);
   }
-  vector<T> res(n);
-  for (int i = 0; i < n; i++) res[ps[i].i] = rs[i];
-  return res;
+  bt1.d.assign(sz, e());
+  bt2.d.assign(sz, e());
+  for (int i = n - 1; i >= 0; i--) {
+    auto [x, y] = ps[i].p;
+    chmin(ans[ps[i].i], min(
+      -bt2.sum(y_id[i]) - (x + y),
+      -bt1.sum(y_id[i]) - (x - y)
+    ));
+    bt2.apply(y_id[i], -(x + y));
+    bt1.apply(y_id[i], -(x - y));
+  }
+  return ans;
 }
