2e2f1fd637f30cbc3d4bc61b5f00ec4cf697e4cf
_ <>
2023-08-30 04:26:01 +0900

    refactor AhoCorasick

--- AhoCorasick.cpp
+++ AhoCorasick.cpp
@@ -2,74 +2,63 @@ struct AhoCorasick {
   static const int csize = 26;
   static const int offset = 'a';
   struct Node {
-    int to[csize]{};
+    int to[csize];
     int flink;
-    int olink;
-    int ends = 0;
-    void mark() { ends++; }
+
+    bool ends = false;
+
+    Node() { memset(to, -1, sizeof(to)); }
+    void mark() { ends = true; }
   };
   vector<Node> nodes;
-  // node 0 : dummy
-  // node 1 : root
-  AhoCorasick() : nodes(2) {}
   void add(const auto& s) {
-    int u = 1;
+    if (nodes.empty()) nodes.emplace_back();
+    int u = 0;
     for (auto si : s) {
       int c = si - offset;
-      if (!nodes[u].to[c]) {
+      int v = nodes[u].to[c];
+      if (v == -1) {
+        v = nodes[u].to[c] = nodes.size();
         nodes.emplace_back();
-        nodes[u].to[c] = int(nodes.size()) - 1;
       }
-      u = nodes[u].to[c];
+      u = v;
     }
     nodes[u].mark();
   }
   void build() {
-    for (int c = 0; c < csize; c++) nodes[0].to[c] = 1;
-    nodes[1].flink = 0;
-    nodes[1].olink = 0;
+    nodes[0].flink = -1;
     queue<int> q;
-    q.emplace(1);
+    q.emplace(0);
     while (!q.empty()) {
       const int u = q.front();
       q.pop();
       for (int c = 0; c < csize; c++) {
         int v = nodes[u].to[c];
-        if (!v) continue;
+        if (v == -1) continue;
         q.emplace(v);
-        // u->v
-        // compute failure link of v
-        int w = nodes[u].flink;
-        while(!nodes[w].to[c]) w = nodes[w].flink;
-        nodes[v].flink = nodes[w].to[c];
-        // compute output link of v
-        int f = nodes[v].flink;
-        nodes[v].olink = nodes[v].ends ? f : nodes[f].olink;
+        int f = next_v(nodes[u].flink, c);
+        nodes[v].flink = f;
+        // problem-specific process
+        nodes[v].ends |= nodes[f].ends;
       }
     }
   }
-  int get_next(int u, auto si) {
-    int c = si - offset;
-    int v = nodes[u].to[c];
-    while(!v) {
-      u = nodes[u].flink;
-      v = nodes[u].to[c];
-    }
-    return v;
-  }
-  auto run(const auto& s) {
-    int u = 1;
-    for (auto si : s) {
-      int c = si - offset;
+  int next_v(int u, int c) {
+    while (u != -1) {
       int v = nodes[u].to[c];
-      while(!v) {
-        u = nodes[u].flink;
-        v = nodes[u].to[c];
-      }
+      if (v != -1) return v;
+      u = nodes[u].flink;
+    }
+    return 0;
+  }
+  int next(int u, auto si) { return next_v(u, si - offset); }
+  auto run(const auto& s) {
+    int u = 0;
+    for (auto si : s) {
+      u = next(u, si);
       if (!u) {
         return;
       }
-      u = v;
     }
   }
 };
