4c8565176270a117906286e3a43ec4a368e0c201
_ <>
2023-12-13 00:30:16 +0900

    change dist type from ull to ll

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,68 +1,26 @@
-template<class T>
-std::vector<unsigned long long> dijkstra(std::vector<std::vector<std::pair<int,T>>>& to, int s=0) {
-    const unsigned long long INF = 1002003004005006007;
-    struct QueElem {
-        int v;
-        unsigned long long c;
-        bool operator<(const QueElem a) const {return c > a.c;}
-        QueElem(int v, unsigned long long c): v(v), c(c) {}
-    };
-    std::priority_queue<QueElem> q;
-    std::vector<unsigned long long> dist(to.size(), INF);
-    dist[s] = 0;
-    q.emplace(s, 0);
-    while(!q.empty()) {
-        QueElem qe = q.top(); q.pop();
-        int u = qe.v;
-        unsigned long long c = qe.c;
-        if (c > dist[u]) continue;
-        for(auto vc: to[u]) {
-            int v = vc.first;
-            unsigned long long nc = c + vc.second;
-            if (nc < dist[v]) {
-                dist[v] = nc;
-                q.emplace(v, nc);
-            }
-        }
+constexpr long long INF = 1002003004005006007;
+template <class T>
+vector<long long> dijkstra(vector<vector<pair<int, T>>>& to, int s = 0) {
+  struct QueElem {
+    int v;
+    long long d;
+    bool operator<(const QueElem& a) const { return d > a.d; }
+    QueElem(int v, long long d) : v(v), d(d) {}
+  };
+  priority_queue<QueElem> q;
+  vector<long long> dist(to.size(), INF);
+  dist[s] = 0;
+  q.emplace(s, 0);
+  while (!q.empty()) {
+    auto [u, d] = q.top(); q.pop();
+    if (d > dist[u]) continue;
+    for (auto [v, dv] : to[u]) {
+      long long nd = d + dv;
+      if (nd < dist[v]) {
+        dist[v] = nd;
+        q.emplace(v, nd);
+      }
     }
-    return dist;
-}
-
-
-
-// returns dists and previous nodes
-const unsigned long long INF = 1002003004005006007;
-template<class T>
-std::pair<std::vector<unsigned long long>, std::vector<int>> dijkstra(std::vector<std::vector<std::pair<int,T>>>& to, int s=0) {
-    struct QueElem {
-        int v;
-        unsigned long long c;
-        bool operator<(const QueElem a) const {return c > a.c;}
-        QueElem(int v, unsigned long long c): v(v), c(c) {}
-    };
-    std::pair<std::vector<unsigned long long>, std::vector<int>> res;
-    std::vector<unsigned long long>& dist = res.first;
-    std::vector<int>& pre = res.second;
-    dist.resize(to.size(), INF);
-    pre.resize(to.size(), -1);
-    std::priority_queue<QueElem> q;
-    dist[s] = 0;
-    pre[s] = -1;
-    q.emplace(s, 0);
-    while(!q.empty()) {
-        QueElem qe = q.top(); q.pop();
-        int u = qe.v;
-        unsigned long long c = qe.c;
-        if (c > dist[u]) continue;
-        for(auto vc: to[u]) {
-            int v = vc.first;
-            unsigned long long nc = c + vc.second;
-            if (nc < dist[v]) {
-                dist[v] = nc;
-                pre[v] = u;
-                q.emplace(v, nc);
-            }
-        }
-    }
-    return res;
+  }
+  return dist;
 }
