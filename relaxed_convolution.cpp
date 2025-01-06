13c52a8f24bc678dcc03724c8a07fa2f0979c55f
_ <>
2025-01-07 04:06:17 +0900

    fix typo in var names and add get method in relaxed convolution

--- relaxed_convolution.cpp
+++ relaxed_convolution.cpp
@@ -1,11 +1,12 @@
 struct relaxed_convolution {
   vector<mint> f{0}, g{0}, fg{0}, tmp1, tmp2;
-  vector<vector<mint>> f_ftt, g_ftt;
+  vector<vector<mint>> f_fft, g_fft;
   int cur = 0;
   void add(mint fi, mint gi) {
-    f[cur] = fi, g[cur] = gi;
+    f[cur] += fi, g[cur] += gi;
     fg[cur] += cur ? fi * g[0] + f[0] * gi : fi * gi;
   }
+  mint get() { return fg[cur]; }
   void proceed(mint fi, mint gi) {
     add(fi, gi);
     proceed();
@@ -16,16 +17,16 @@ struct relaxed_convolution {
       int n2 = 2 * cur;
       f.resize(n2), g.resize(n2), fg.resize(n2);
       if (cur == 1) return;
-      f_ftt.emplace_back(), g_ftt.emplace_back();
-      swap(f_ftt.back(), tmp1), swap(g_ftt.back(), tmp2);
+      f_fft.emplace_back(), g_fft.emplace_back();
+      swap(f_fft.back(), tmp1), swap(g_fft.back(), tmp2);
       tmp1.resize(n2), tmp2.resize(n2);
       for (int i = 0; i < cur; i++) tmp1[i] = f[i], tmp2[i] = g[i];
       internal::butterfly(tmp1), internal::butterfly(tmp2);
-      f_ftt.back().assign(tmp1.begin(), tmp1.begin() + cur);
-      g_ftt.back().assign(tmp2.begin(), tmp2.begin() + cur);
+      f_fft.back().assign(tmp1.begin(), tmp1.begin() + cur);
+      g_fft.back().assign(tmp2.begin(), tmp2.begin() + cur);
       mint iz = mint::raw(mint::mod() - (mint::mod() - 1) / cur);
-      for (auto& x : f_ftt.back()) x *= iz;
-      for (auto& x : g_ftt.back()) x *= iz;
+      for (auto& x : f_fft.back()) x *= iz;
+      for (auto& x : g_fft.back()) x *= iz;
       for (int i = 0; i < n2; i++) tmp1[i] *= tmp2[i];
       internal::butterfly_inv(tmp1);
       iz = mint::raw(mint::mod() - (mint::mod() - 1) / n2);
@@ -35,14 +36,14 @@ struct relaxed_convolution {
       {
         tmp1.assign(f.begin() + l, f.begin() + cur), tmp1.resize(n2);
         internal::butterfly(tmp1);
-        for (auto it = tmp1.begin(); auto x : g_ftt[k]) *it++ *= x;
+        for (auto it = tmp1.begin(); auto x : g_fft[k]) *it++ *= x;
         internal::butterfly_inv(tmp1);
         for (int i = n; i < n2; i++) fg[l + i] += tmp1[i];
       }
       {
         tmp1.assign(g.begin() + l, g.begin() + cur), tmp1.resize(n2);
         internal::butterfly(tmp1);
-        for (auto it = tmp1.begin(); auto x : f_ftt[k]) *it++ *= x;
+        for (auto it = tmp1.begin(); auto x : f_fft[k]) *it++ *= x;
         internal::butterfly_inv(tmp1);
         for (int i = n; i < n2; i++) fg[l + i] += tmp1[i];
       }
